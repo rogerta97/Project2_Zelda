b4208dc239f59d47c706fa2fd0e2d3a7fd70b4d6
Trodek <ibanmas@gmail.com>
2017-02-23 16:04:52 +0100

    minion wip

---
+++ Motor2D/Minion.h
@@ -0,0 +1,21 @@
+#ifndef _MINION_H_
+#define _MINION_H_
+
+#include "Entity.h"
+
+class Minion :public Entity 
+{
+public:
+
+	//Minion();
+
+private:
+
+public:
+
+private:
+
+};
+
+#endif // !MINION_H_
+
--- Motor2D/j1Entity.h
+++ Motor2D/j1Entity.h
@@ -11,7 +11,7 @@ class PhysBody;
 
 enum entity_name
 {
-	link,
+	link, minion
 };
 
 class Entity;
