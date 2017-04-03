485f1d8ffdc2135ab50808161b0b07a231d90973
Trodek <ibanmas@gmail.com>
2017-04-03 20:46:12 +0200

    Char select music

--- Motor2D/CharacterSelectionScene.cpp
+++ Motor2D/CharacterSelectionScene.cpp
@@ -5,6 +5,7 @@
 #include "p2Log.h"
 #include "j1Scene.h"
 #include "j1Viewports.h"
+#include "j1Audio.h"
 
 #define CHARACTER_NUM 3
 
@@ -24,6 +25,8 @@ bool CharacterSelectionScene::Start()
 	window = App->gui->UI_CreateWin(iPoint(0,0), w, h, 2, false); 
 
 	CreateScene(w,h); 
+
+	App->audio->PlayMusic("Audio/Music/guessing-game_house.ogg");
 
 	return true;
 }
--- Motor2D/j1Audio.h
+++ Motor2D/j1Audio.h
@@ -3,7 +3,7 @@
 
 #include "j1Module.h"
 
-#define DEFAULT_MUSIC_FADE_TIME 2.0f
+#define DEFAULT_MUSIC_FADE_TIME 0.0f
 
 struct _Mix_Music;
 struct Mix_Chunk;
