e2a426e41bbc1fb3be406951a71d093e2dd08582
rogerta97 <rogerteello97@gmail.com>
2017-04-04 22:59:38 +0200

    CharacterSelection BugFixing

--- Motor2D/CharacterSelectionScene.cpp
+++ Motor2D/CharacterSelectionScene.cpp
@@ -1,7 +1,5 @@
 #include "CharacterSelectionScene.h"
 #include "j1Console.h"
-
-#include "CharacterSelectionScene.h"
 #include "j1Gui.h"
 #include "j1Window.h"
 #include "j1Input.h"
@@ -459,9 +457,7 @@
 //	win.description.push_back(window->CreateText(iPoint(110, 120), App->font->game_font_12, 25, false, 255, 255, 255));
 //	win.description.at(1)->SetText("Sword Swing    Link swings his sword around him and\n damage all nearby enemies");
 //	win.description.push_back(window->CreateText(iPoint(110, 200), App->font->game_font_12, 25, false, 255, 255, 255));
-//	win.description.at(2)->SetText("Boomerang    Link shoots his boomerang, catch it\n when it comes back!");
-//	win.description.push_back(window->CreateText(iPoint(110, 270), App->font->game_font_12, 25, false, 255, 255, 255));
-//	win.description.at(3)->SetText("Charge    Link will run to target location ignoring\n collisions and damaging all enemies he touches.");
+
 //
 //	int f = 0; 
 //	while (f < 4) 
@@ -484,16 +480,7 @@
 //	win.habilites.push_back(window->CreateImage(iPoint(60, 200), { 284, 195, 40, 40 }, false));
 //	win.habilites.push_back(window->CreateImage(iPoint(60, 270), { 284, 195, 40, 40 }, false));
 //
-//	win.description.push_back(window->CreateText(iPoint(110, 70), App->font->game_font_12, 25, false, 255, 255, 255));
-//	win.description.at(0)->SetText("Basic Attack    Frontal smash with the trident");
-//	win.description.push_back(window->CreateText(iPoint(110, 130), App->font->game_font_12, 25, false, 255, 255, 255));
-//	win.description.at(1)->SetText("Bat    Ganon shoot a bat to target direction");
-//	win.description.push_back(window->CreateText(iPoint(110, 200), App->font->game_font_12, 25, false, 255, 255, 255));
-//	win.description.at(2)->SetText("Flame Shield     Ganon puts a shield to himself and\n summon 3 fire orbs that orbit around him. ");
-//	win.description.push_back(window->CreateText(iPoint(110, 270), App->font->game_font_12, 25, false, 255, 255, 255));
-//	win.description.at(3)->SetText("Evil Jump    Ganaon jumps to a target position and\n stuns all enemies on the area.");
-//
-//	f = 0;
+
 //	while (f < 4)
 //	{
 //		info_container[f++].push_back(win);
@@ -513,16 +500,7 @@
 //	win.habilites.push_back(window->CreateImage(iPoint(60, 120), { 284, 195, 40, 40 }, false));
 //	win.habilites.push_back(window->CreateImage(iPoint(60, 200), { 284, 195, 40, 40 }, false));
 //	win.habilites.push_back(window->CreateImage(iPoint(60, 270), { 284, 195, 40, 40 }, false));
-//
-//	win.description.push_back(window->CreateText(iPoint(110, 70), App->font->game_font_12, 25, false, 255, 255, 255));
-//	win.description.at(0)->SetText("Basic Attack    Navi shoots an energy ball in front\n of her that hits the first enemy hit");
-//	win.description.push_back(window->CreateText(iPoint(110, 130), App->font->game_font_12, 25, false, 255, 255, 255));
-//	win.description.at(1)->SetText("Watch Out!    Navi heals allies around her. Range is\n displayed as a circle with Navi as centre.");
-//	win.description.push_back(window->CreateText(iPoint(110, 200), App->font->game_font_12, 25, false, 255, 255, 255));
-//	win.description.at(2)->SetText("Blink     Navi uses her magic energy to teleport a\n small distance in front of her.");
-//	win.description.push_back(window->CreateText(iPoint(110, 270), App->font->game_font_12, 25, false, 255, 255, 255));
-//	win.description.at(3)->SetText("Hey Listen!    disable enemy players view for 3\n seconds");
-//
+
 //	f = 0;
 //	while (f < 4)
 //	{
@@ -716,13 +694,57 @@ bool CharacterSelectionScene::Start()
 
 	// Create players and info
 	link = new player_data();
-	link->ability1_text = "h";
-	players_data.push_back(link);
+	link->ability1_text = "Basic Attack    Frontal smash with the sword";
+	link->ability2_text = "Sword Swing    Link swings his sword around him and\n damage all nearby enemies"; 
+	link->ability3_text = "Boomerang    Link shoots his boomerang, catch it\n when it comes back!";
+	link->ability4_text = "Charge    Link will run to target location ignoring\n collisions and damaging all enemies he touches.";
+	link->big_image = {0, 294, 116, 147}; 
+	link->small_image = {348, 445, 74, 90};
+	link->name = "link"; 
+	link->entity = entity_name::link; 
+
+	ganon = new player_data();
+	ganon->ability1_text = "Basic Attack    Frontal smash with the trident";
+	ganon->ability2_text = "Bat    Ganon shoot a bat to target direction";
+	ganon->ability3_text = "Flame Shield     Ganon puts a shield to himself and\n summon 3 fire orbs that orbit around him. ";
+	ganon->ability4_text = "Evil Jump    Ganaon jumps to a target position and\n stuns all enemies on the area.";
+	ganon->big_image = { 116, 294, 116, 147 };
+	ganon->small_image = { 422, 445, 75, 90 };
+	ganon->name = "ganon";
+	ganon->entity = entity_name::ganon;
+
+	navi = new player_data();
+	navi->ability1_text = "Basic Attack    Navi shoots an energy ball in front\n of her that hits the first enemy hit";
+	navi->ability2_text = "Watch Out!    Navi heals allies around her. Range is\n displayed as a circle with Navi as centre.";
+	navi->ability3_text = "Blink     Navi uses her magic energy to teleport a\n small distance in front of her.!";
+	navi->ability4_text = "Hey Listen!    disable enemy players view for 3\n seconds";
+	navi->big_image = { 232, 294, 116, 147 };
+	navi->small_image = { 496, 445, 75, 90 };
+	navi->name = "navi";
+	navi->entity = entity_name::navi;
 
 	curr_player_data1 = link;
-	curr_player_data2 = link;
-	curr_player_data3 = link;
+	curr_player_data2 = ganon;
+	curr_player_data3 = navi;
 	curr_player_data4 = link;
+
+	// ----
+
+	// Creating viewports
+
+	viewport_data data_view_1(1); 
+	viewport_data data_view_2(2);
+	viewport_data data_view_3(3);
+	viewport_data data_view_4(4);
+
+	viewports_data.push_back(data_view_1);
+	viewports_data.push_back(data_view_2);
+	viewports_data.push_back(data_view_3);
+	viewports_data.push_back(data_view_4);
+
+	// ----
+
+	
 
 	return ret;
 }
@@ -740,39 +762,118 @@ bool CharacterSelectionScene::Update(float dt)
 	
 	if(change_scene)
 	{
-		for (int i = 0; i < 4; i++)
-		{
-			App->scene->players[i].character = entity_name::link;
+
+		for (int i = 0; i < 4; i++) {
+			App->scene->players[i].character = entity_name::link; 
 		}
+		
+		/*App->scene->players[0].character = curr_player_data1->entity;
+		App->scene->players[1].character = curr_player_data2->entity;
+		App->scene->players[2].character = curr_player_data3->entity;
+		App->scene->players[3].character = curr_player_data4->entity;*/
+		
 		App->scene->ChangeScene((Scene*)App->scene->main_scene);
 	}
 
 	for (int i = 0; i < viewports_data.size(); i++)
 	{
 		// Take curr
+
 		player_data* curr = nullptr;
 
 		switch (i)
 		{
-		case 1:
+		case 0:
 			curr = curr_player_data1;
 			break;
-		case 2:
+		case 1:
 			curr = curr_player_data2;
 			break;
-		case 3:
+		case 2:
 			curr = curr_player_data3;
 			break;
-		case 4:
+		case 3:
 			curr = curr_player_data4;
 			break;
 		}
 
-		viewports_data[i].abilities_info1->SetText(curr->ability1_text);
+		if (App->input->GetControllerButton(App->scene->players[i].gamepad - 1, SDL_CONTROLLER_BUTTON_LEFTSHOULDER) == KEY_DOWN && viewports_data[i].is_ready == false)
+		{
+			switch (i)
+			{
+			case 0:
+				curr_player_data1 = MoveCharacter("left", curr->entity);
+				break;
+			case 1:
+				curr_player_data2 = MoveCharacter("left", curr->entity);
+				break;
+			case 2:
+				curr_player_data3 = MoveCharacter("left", curr->entity);
+				break;
+			case 3:
+				curr_player_data4 = MoveCharacter("left", curr->entity);
+				break;
+			}
+			
+		}
 
+		if (App->input->GetControllerButton(App->scene->players[i].gamepad - 1, SDL_CONTROLLER_BUTTON_RIGHTSHOULDER) == KEY_DOWN && viewports_data[i].is_ready == false)
+		{
+			switch (i)
+			{
+			case 0:
+				curr_player_data1 = MoveCharacter("right", curr->entity);
+				break;
+			case 1:
+				curr_player_data2 = MoveCharacter("right", curr->entity);
+				break;
+			case 2:
+				curr_player_data3 = MoveCharacter("right", curr->entity);
+				break;
+			case 3:
+				curr_player_data4 = MoveCharacter("right", curr->entity);
+				break;
+			}
 
-		// If Right shoulder
-		//change curr_player_data_x;
+		}
+
+		if (App->input->GetControllerButton(App->scene->players[i].gamepad - 1, SDL_CONTROLLER_BUTTON_Y) == KEY_DOWN)
+		{
+			viewports_data[i].background_info_image->enabled = true; 
+
+			viewports_data[i].abilities_info1->enabled = true; 
+			viewports_data[i].abilities_info2->enabled = true;
+			viewports_data[i].abilities_info3->enabled = true;
+			viewports_data[i].abilities_info4->enabled = true;
+		}
+
+		if (App->input->GetControllerButton(App->scene->players[i].gamepad - 1, SDL_CONTROLLER_BUTTON_Y) == KEY_UP)
+		{
+			viewports_data[i].background_info_image->enabled = false;
+
+			viewports_data[i].abilities_info1->enabled = false;
+			viewports_data[i].abilities_info2->enabled = false;
+			viewports_data[i].abilities_info3->enabled = false;
+			viewports_data[i].abilities_info4->enabled = false;
+		}
+
+		if (App->input->GetControllerButton(App->scene->players[i].gamepad - 1, SDL_CONTROLLER_BUTTON_START) == KEY_DOWN && viewports_data[i].is_ready == false) 
+		{
+			viewports_data[i].is_ready = true; 
+			viewports_data[i].ready_text->SetPos(iPoint(viewports_data[i].ready_text->GetPos().x + 15, viewports_data[i].ready_text->GetPos().y));
+		}
+
+		if (App->input->GetControllerButton(App->scene->players[i].gamepad - 1, SDL_CONTROLLER_BUTTON_B) == KEY_DOWN && viewports_data[i].is_ready == true)
+		{
+			viewports_data[i].is_ready = false;
+			viewports_data[i].ready_text->SetPos(iPoint(viewports_data[i].ready_text->GetPos().x - 15, viewports_data[i].ready_text->GetPos().y));
+		}
+
+		viewports_data[i].SetViewportInfo(curr);
+
+		if (viewports_data[0].is_ready == true && viewports_data[1].is_ready == true && viewports_data[2].is_ready == true && viewports_data[3].is_ready == true)
+			change_cene = true; 
+	
 	}
 
 	return ret;
@@ -780,9 +881,85 @@ bool CharacterSelectionScene::Update(float dt)
 
 bool CharacterSelectionScene::CleanUp()
 {
-	bool ret = true;
+	for (int i = 0; i < 4; i++)
+	{
+		if (App->scene->GetCurrentScene() != App->scene->charselect_screen)
+			App->gui->DeleteElement(viewports_data[i].window);
+	}
+		
+		viewports_data.clear(); 
+			
+		change_scene = false;
+	
+		//Stop Music
+		App->audio->StopMusic();
+	
+		return true;
+}
+
+player_data* CharacterSelectionScene::MoveCharacter(const char * direction, entity_name middle)
+{
+	switch (middle) 
+	{
+	case entity_name::link: 
+		if (direction == "left")
+			return ganon; 
+		else
+			return navi;
+		break; 
+
+	case entity_name::ganon:
+		if (direction == "left")
+			return navi;
+		else 
+			return link;
+		break;
+
+	case entity_name::navi:
+		if (direction == "left")
+			return link;
+		else
+			return ganon;
+		break;
+	}
+
+}
 
 
+bool viewport_data::SetViewportInfo(player_data * player)
+{
 
-	return ret;
+	 big_image->image = player->big_image;
+
+	 if (player->entity == entity_name::link) {
+		 small_image_left->image = App->scene->charselect_screen->ganon->small_image;
+		 small_image_right->image = App->scene->charselect_screen->navi->small_image;
+	 }
+	 else if(player->entity == entity_name::ganon)
+	 {
+		 small_image_left->image = App->scene->charselect_screen->navi->small_image;
+		 small_image_right->image = App->scene->charselect_screen->link->small_image;
+	 }
+	 else
+	 {
+		 small_image_left->image = App->scene->charselect_screen->link->small_image;
+		 small_image_right->image = App->scene->charselect_screen->ganon->small_image;
+	 }
+
+	 text_name->SetText(player->name);
+
+	 abilities_info1->SetText(player->ability1_text);
+	 abilities_info2->SetText(player->ability2_text);
+	 abilities_info3->SetText(player->ability3_text);
+	 abilities_info4->SetText(player->ability4_text);
+
+	 if (is_ready == false)
+		 ready_text->SetText("Press START when ready");
+
+	 else {
+		 ready_text->SetText("READY");
+	 }
+		
+
+	return false;
 }
--- Motor2D/CharacterSelectionScene.h
+++ Motor2D/CharacterSelectionScene.h
@@ -129,23 +129,26 @@ struct player_data
 struct viewport_data
 {
 	viewport_data() {};
+
 	viewport_data(int viewport)
 	{
 		SDL_Rect view = App->view->GetViewportRect(1);
-		iPoint big_image_pos = { (int)(view.w*0.5), (int)(view.h*0.5) };
-		iPoint small_image_left_pos = {};
-		iPoint small_image_right_pos = {};
+		iPoint big_image_pos = { 180, 70 };
+		iPoint small_image_left_pos = {80, 100};
+		iPoint small_image_right_pos = {320, 100};
 
-		iPoint button_info_pos = {};
-		iPoint background_info_image_pos = {};
+		iPoint button_info_pos = {350, 310};
+		iPoint background_info_image_pos = {10, 10};
 
-		iPoint background_name_image_pos = {};
-		iPoint text_name_pos = {};
+		iPoint background_name_image_pos = {200, 200};
+		iPoint text_name_pos = {250, 210};
 
-		iPoint abilities_info1 = {};
-		iPoint abilities_info2 = {};
-		iPoint abilities_info3 = {};
-		iPoint abilities_info4 = {};
+		iPoint abilities_info1_pos = {15, 20};
+		iPoint abilities_info2_pos = { 15, 30};
+		iPoint abilities_info3_pos = { 15, 40};
+		iPoint abilities_info4_pos = { 15, 50};
+
+		iPoint ready_text_pos = { 250, 270 }; 
 
 		window = App->gui->UI_CreateWin(iPoint(0, 0), view.w, view.h, 1, true, false, true);
 		window->viewport = viewport;
@@ -153,12 +156,34 @@ struct viewport_data
 		big_image = window->CreateImage(big_image_pos, { 0, 0, 0, 0 });
 		small_image_left = window->CreateImage(small_image_left_pos, { 0, 0, 0, 0 });
 		small_image_right = window->CreateImage(small_image_right_pos, { 0, 0, 0, 0 });
-		button_info = window->CreateImage(button_info_pos, { 0, 0, 0, 0 });
 
+		button_info = window->CreateImage(button_info_pos, { 324, 195, 95, 40 });
+
+		background_name_image = window->CreateImage(background_name_image_pos, { 128, 52,  217, 55 });
+		text_name = window->CreateText(text_name_pos, App->font->game_font);
+
+		ready_text = window->CreateText(ready_text_pos, App->font->game_font);
+
+		background_info_image = window->CreateImage(background_info_image_pos, { 656, 595, 470, 325 });
+		background_info_image->enabled = false;
+
+		abilities_info1 = window->CreateText(abilities_info1_pos, App->font->game_font);
+		abilities_info1->enabled = false;
+		abilities_info2 = window->CreateText(abilities_info2_pos, App->font->game_font);
+		abilities_info2->enabled = false;
+		abilities_info3 = window->CreateText(abilities_info3_pos, App->font->game_font);
+		abilities_info3->enabled = false;
+		abilities_info4 = window->CreateText(abilities_info4_pos, App->font->game_font);
+		abilities_info4->enabled = false;
+
+		
 	};
+
 	~viewport_data() {};
 
 	int viewport = 0;
+	bool is_ready = false; 
+
 	UI_Window* window = nullptr;
 	UI_Image* big_image = nullptr;
 	UI_Image* small_image_left = nullptr;
@@ -174,6 +199,10 @@ struct viewport_data
 	UI_Text* abilities_info2 = nullptr;
 	UI_Text* abilities_info3 = nullptr;
 	UI_Text* abilities_info4 = nullptr;
+
+	UI_Text* ready_text = nullptr; 
+
+	bool	 SetViewportInfo(player_data* player); 
 };
 
 class CharacterSelectionScene : public Scene
@@ -186,12 +215,17 @@ public:
 	bool CleanUp();
 
 private:
+
+	player_data* MoveCharacter(const char* direction, entity_name middle);
+
 public:
-private:
-	vector<viewport_data> viewports_data;
-	vector<player_data*>  players_data;
 
 	player_data* link = nullptr;
+	player_data* ganon = nullptr;
+	player_data* navi = nullptr;
+
+private:
+	vector<viewport_data> viewports_data;
 
 	player_data* curr_player_data1 = nullptr;
 	player_data* curr_player_data2 = nullptr;
