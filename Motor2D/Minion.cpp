cbc8cee9d44bba04ece76a41a12fc9778b76d331
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2017-04-04 19:53:18 +0200

    Minion collisions changed

--- Motor2D/Minion.cpp
+++ Motor2D/Minion.cpp
@@ -24,7 +24,8 @@ Minion::Minion(iPoint pos)
 {
 	game_object = new GameObject(iPoint(pos.x, pos.y), iPoint(30,40), App->cf->CATEGORY_PLAYER, App->cf->MASK_PLAYER, pbody_type::p_t_npc, 0);
 
-	game_object->CreateCollision(iPoint(0, 0), game_object->GetHitBoxSize().x, game_object->GetHitBoxSize().y, fixture_type::f_t_hit_box);
+	game_object->CreateCollisionSensor(iPoint(0, 0), game_object->GetHitBoxSize().x, game_object->GetHitBoxSize().y, fixture_type::f_t_hit_box);
+	game_object->CreateCollision(iPoint(0, 15), 7, fixture_type::f_t_collision_box);
 	game_object->SetListener((j1Module*)App->entity);
 	game_object->SetFixedRotation(true);
 
