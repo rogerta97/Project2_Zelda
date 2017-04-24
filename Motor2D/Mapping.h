9a1356cb904c73e3f95f9b4e7dced1587d5d08c0
Trodek <ibanmas@gmail.com>
2017-04-24 23:19:25 +0200

    mapping wip

---
+++ Motor2D/Mapping.h
@@ -0,0 +1,36 @@
+#ifndef _MAPPING_H_
+#define _MAPPING_H_
+
+enum map_keys
+{
+	m_k_ability1,
+	m_k_ability2,
+	m_k_ability3,
+	m_k_ability4,
+	m_k_confirm,
+	m_k_back,
+	m_k_shop,
+	m_k_minimap,
+	m_k_null,
+};
+
+class Mapping
+{
+public:
+	Mapping();
+	~Mapping();
+
+	MapKey(map_keys key, )
+
+private:
+
+public:
+
+private:
+	map_keys* mapping = nullptr;
+
+
+};
+
+
+#endif
