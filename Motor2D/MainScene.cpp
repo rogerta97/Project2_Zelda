d58923663318eef860c1c353584bc07093022724
Trodek <ibanmas@gmail.com>
2017-05-10 18:26:57 +0200

    zelda progress bar update correctly

--- Motor2D/MainScene.cpp
+++ Motor2D/MainScene.cpp
@@ -583,15 +583,14 @@ void MainScene::UpdateProgressBar()
 {
 	iPoint zelda_pos = App->map->WorldToMap(zelda_manager->GetZeldaPos().x, zelda_manager->GetZeldaPos().y);
 
-	float percentage = (zelda_pos.x-36) * 100 / 95;
+	float percentage = (zelda_pos.x-35) * 100 / 97;
 	percentage /= 100;
 
-	int delta = (progress_bar->rect.w * percentage) - progress_bar->rect.w/2;
+	int delta = (progress_bar->rect.w * percentage);
 
-	for (vector<MainSceneViewport>::iterator it = ui_viewports.begin(); it != ui_viewports.end(); it++)
-	{
-		//(*it)->princess->SetPos({ (*it)->progress_bar->GetPos().x + delta, (*it)->progress_bar->GetPos().y - 4});
-	}
+	
+	princess->SetPos({ progress_bar->GetPos().x + delta - princess->image.w/2, progress_bar->GetPos().y - 4});
+	
 	
 
 }
