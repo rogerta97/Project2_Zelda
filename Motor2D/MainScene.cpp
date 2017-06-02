7183a699c065fcfd3d521cdd751bedc7d8a56eb4
GuillemD <gdrdvilla@gmail.com>
2017-06-02 19:17:03 +0200

    Volume fixing

--- Motor2D/MainScene.cpp
+++ Motor2D/MainScene.cpp
@@ -339,7 +339,7 @@ bool MainScene::Start()
 	App->console->AddCommand("scene.set_player_gamepad", App->scene, 2, 2, "Set to player the gampad number. Min_args: 2. Max_args: 2. Args: 1, 2, 3, 4");
 	App->console->AddCommand("scene.set_player_camera", App->scene, 2, 2, "Set to player the camera number. Min_args: 2. Max_args: 2. Args: 1, 2, 3, 4");
 
-	App->audio->ChangeVolume(25);
+	App->audio->ChangeVolume(5);
 	App->audio->PlayMusic("Audio/Music/overworld.ogg");
 
 	return ret;
@@ -627,7 +627,7 @@ void MainScene::EndGame(int _winner)
 
 	UpdateWinnerAnim(winner,0.0f);
 
-	App->audio->ChangeVolume(5);
+	App->audio->ChangeVolume(75);
 	App->audio->PlayMusic("Audio/Music/triforce_chamber.ogg");
 
 	game_timer->Start();
